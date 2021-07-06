09353cbe6bb02df9b64133dc0588a3cc079108d2
Deepak Singh <deepaksingh@Deepaks-MacBook-Air.local>
2021-07-06 16:59:29 +0530

    refactore

--- block-BNaacb/sample/model/article.js
+++ block-BNaacb/sample/model/article.js
@@ -2,6 +2,6 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var articleSchema = new Schema({
-    name : String,
-    age : Number
+    title : String,
+    count : Number
 })
