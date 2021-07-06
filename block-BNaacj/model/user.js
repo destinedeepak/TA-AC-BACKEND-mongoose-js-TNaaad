e2f9ed4c34117b373b6e2c8a4abd9847cc5fea57
Deepak Singh <deepaksingh@Deepaks-MacBook-Air.local>
2021-07-06 18:52:49 +0530

    refactor

--- block-BNaacj/model/user.js
+++ block-BNaacj/model/user.js
@@ -1,14 +1,11 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var userSchema = new Schema(
-  {
-    name: String,
-    age: Number,
-    favorites: [String],
-    marks: [Number],
-    password: { type: String, min: 5, max: 15 },
-    date: { type: date, default: new Date() },
-  },
-  { timestamps: true }
-);
+var userSchema = new Schema({
+  name: String,
+  age: Number,
+  favorites: [String],
+  marks: [Number],
+  password: { type: String, minlength: 5, maxlength: 15 },
+  createdAt: { type: Date, default: new Date() },
+});
